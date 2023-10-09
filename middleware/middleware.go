69998f8004f85daa74275523c11a7815e647351e
Ayanda Jeremiah Ayoku <Olumaintain619@gmail.com>
2023-10-09 23:01:01 +0100

    work on CORs

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -7,7 +7,7 @@ import (
 // CorsMiddleware handles the CORS middleware
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://, https://")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Origin, Accept, Content-Type, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
